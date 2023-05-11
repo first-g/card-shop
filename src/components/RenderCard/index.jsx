a8167573e33b677326dd4313274804985f460e96
first-g <gamerfreestyle14@gmail.com>
2023-05-11 21:00:58 +0600

    add counter and total sum

--- src/components/RenderCard/index.jsx
+++ src/components/RenderCard/index.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './RenderCard.css'
 import { dataProduct } from '../../data/data';
 
 const RenderCard = () => {
-    let product = dataProduct.map((item) => {
+    let [counts, setCounts] = useState(dataProduct.map(() => 0));
+
+    const incrementCount = (index) => {
+        let newCounts = [...counts];
+        newCounts[index]++;
+        setCounts(newCounts)
+    }
+
+    const decrementCount = (index) => {
+        let newCounts = [...counts];
+        if (newCounts[index] > 0) {
+            newCounts[index]--;
+            setCounts(newCounts)
+        }
+    }
+
+    let product = dataProduct.map((item, index) => {
         return (
-            <div className='card-wrapper'>
+            <div className='card-wrapper' key={item.id}>
                 <img src={item.img} alt="tovar" />
                 <div className="description-section">
                     <h3>{item.title}</h3>
@@ -13,12 +29,12 @@ const RenderCard = () => {
                     <p></p>
                 </div>
                 <div className="counter-section">
-                    <button>+</button>
-                    <span>1</span>
-                    <button>-</button>
+                    <button onClick={() => incrementCount(index)}>+</button>
+                    <span>{counts[index]}</span>
+                    <button onClick={() => decrementCount(index)}>-</button>
                 </div>
                 <div className="price-section">
-                    <p>{item.price}</p>
+                    <p>{item.price}$</p>
                     <span>Save for later</span>
                     <br />
                     <span>Remove</span>
@@ -26,7 +42,23 @@ const RenderCard = () => {
             </div>
         )
     })
-  return product
+    return (
+        <>
+            {product}
+            <div className='total'>
+                <div className="total__body">
+                    <div className="total__block">
+                        <div>
+                            <h3>Sub-Total</h3>
+                            <p>{1} items</p>
+                        </div>
+                        <h2 className='total__price'>6$</h2>
+                    </div>
+                    <button>Checkout</button>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default RenderCard
--- src/data/data.js
+++ src/data/data.js
@@ -1,13 +1,13 @@
 export const dataProduct = [
     {
-        id: 'productCola',
+        id: 1,
         img: './assets/cola-bottle.png',
         title: 'CocaCola',
         capacity: '250ml',
         price: 2.99
     },
     {
-        id: 'productPepsi',
+        id: 2,
         img: './assets/pepsi-bottle.png',
         title: 'Pepsi',
         capacity: '250ml',
