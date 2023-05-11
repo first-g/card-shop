aead129c6b6827d75319c7ee47fbf6ef6f3a216d
first-g <gamerfreestyle14@gmail.com>
2023-05-11 17:16:53 +0600

    компоненты для Тунук

---
+++ src/components/Buttons/Checkout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Buttons.css'
+
+const Checkout = () => {
+  return (
+    <div>Checkout</div>
+  )
+}
+
+export default Checkout
---
+++ src/components/Buttons/PlusMinus.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Buttons.css'
+
+const PlusMinus = () => {
+  return (
+    <div>PlusMinus</div>
+  )
+}
+
+export default PlusMinus
