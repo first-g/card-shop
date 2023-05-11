22b59e39b9be5a279e360b4d24db35af99446ae1
first-g <gamerfreestyle14@gmail.com>
2023-05-11 20:14:09 +0600

    add main block

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import RenderCard from './components/RenderCard';
 function App() {
   return (
     <div className="App">
-      hello 
-      <RenderCard/>
+      <div className='container'>
+        <div className='app__body'>
+          <div className='app__header'>
+            <h1>Shopping cart</h1>
+            <button>Remove all</button>
+          </div>
+          <RenderCard />
+        </div>
+      </div>
     </div>
   );
 }
